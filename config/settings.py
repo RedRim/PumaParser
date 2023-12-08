542f87265f7ae13e65c11f563ea065646e9e2f07
Степан Черныx <stepanchernykh@mail.ru>
2023-12-08 14:08:54 +0800

    included postgresql db

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'parser.apps.ParserConfig',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -65,8 +66,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'parser',
+        'USER': 'parser',
+        'PASSWORD': 'parser',
     }
 }
 
